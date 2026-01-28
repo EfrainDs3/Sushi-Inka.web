bf9719b68b92f43b3877a13ea935e5aa6a61fbc3
EfrainDs3 <santiagotorresmurrieta@gmail.com>
2026-01-28 17:46:45 -0500

    Primer commit del proyecto

---
+++ src/main/java/web/Sushi_Inka/SushiInkaApplication.java
@@ -0,0 +1,13 @@
+package web.Sushi_Inka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiInkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiInkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/Sushi_Inka/SushiInkaApplicationTests.java
@@ -0,0 +1,13 @@
+package web.Sushi_Inka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiInkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
